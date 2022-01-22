079315e8667f15c36b79017bfb181c7850822867
Timothy Yoon <timsyoon@gmail.com>
2022-01-21 20:41:11 -0800

    Add tests for other matchers

--- calculator.test.js
+++ calculator.test.js
@@ -23,3 +23,41 @@ describe("Calculator tests", () => {
         expect(result).toBe(16);
     });
 });
+
+describe("Matcher tests", () => {
+    test("Equality matchers", () => {
+        expect(2 * 2).toBe(4);
+        expect(4 - 2).not.toBe(1);
+    });
+    test("Truthy matchers", () => {
+        var name = "Bob"
+        var nl = null;
+
+        expect(nl).toBeNull();
+        expect(name).not.toBeNull();
+        
+        // name has a valid value
+        expect(name).toBeTruthy();
+
+        // failure
+        expect(nl).toBeTruthy();
+
+        // pass
+        expect(nl).toBeFalsy();
+        expect(0).toBeFalsy();
+    });
+    test("Numeric matchers", () => {
+        var num1 = 100;
+        var num2 = -20;
+        var num3 = 0;
+
+        expect(num1).toBeGreaterThan(10);
+        expect(num2).toBeLessThanOrEqual(0);
+        expect(num3).toBeGreaterThanOrEqual(0);
+    });
+    test("String matchers", () => {
+        var string1 = "Dan took the deep dive down the rabbit hole"
+        expect(string1).toMatch(/dive/);
+        expect(string1).not.toMatch(/abc/);
+    });
+});
